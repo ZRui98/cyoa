cdcb8aef5e319c70a0b43e08e0e67bdcd614a6a4
Rui Zhu <ZRui98@users.noreply.github.com>
2023-06-20 23:45:31 -0400

    add google auth to backend and sign in button to front end (#5)

--- client/src/routes/(app)/login/+page.svelte
+++ client/src/routes/(app)/login/+page.svelte
@@ -1,1 +1,37 @@
-<div>Login page</div>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import {env} from '$env/dynamic/public';
+
+    let googleButton: HTMLElement;
+    const initGoogleButton = async () => {
+        console.log('ready');
+        google.accounts.id.initialize({
+            client_id: env.PUBLIC_GOOGLE_CLIENT_ID,
+            ux_mode: 'redirect',
+            login_uri: 'http://localhost:3000/auth/google/callback'
+        });
+        google.accounts.id.renderButton(
+            googleButton,
+            {
+                theme: 'outline',
+                size: 'large',
+                type: 'standard',
+                shape: 'pill',
+                width: '250',
+            }
+        );
+    }
+    // onMount();
+
+</script>
+
+<svelte:head>
+    <script src="https://accounts.google.com/gsi/client" on:load={initGoogleButton}></script>
+</svelte:head>
+
+<main>
+    <div id=buttonDiv bind:this={googleButton}></div>
+</main>
+
+<style global>
+</style>
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,18 @@
 import cors from '@fastify/cors'
 import multipart from '@fastify/multipart';
+import formbody from '@fastify/formbody';
+import passport from '@fastify/passport';
+import fastifySecureSession from '@fastify/secure-session';
+
 import fastify from 'fastify';
 import { default as adventureRoutes } from './routes/adventure';
 import { default as assetRoutes } from './routes/asset';
 import { default as userRoutes } from './routes/user';
+import { default as authRoutes } from './routes/auth';
 import { adventureMetadataSchema, adventureSchema } from './models/Adventure';
+import path from 'path';
+import * as fs from 'fs';
+import { SignInWithGoogleStrategy } from './api/middleware/SignInWithGoogleStrategy';
 
 const app = fastify({
   logger: true,
@@ -21,15 +29,49 @@ app
 
 
 // plugins
+// auth
+app.register(fastifySecureSession, {
+  key: Buffer.from(`${process.env.SECRET_SESSION_KEY}`, 'hex'),
+  cookie: {
+    path: '/',
+    sameSite: 'strict',
+  }
+}).register(passport.initialize())
+.register(passport.secureSession());
+
+passport.use('sign-in-with-google', new SignInWithGoogleStrategy({
+  clientID: `${process.env.GOOGLE_SIGN_IN_CLIENT_ID}`,
+  clientSecret: `${process.env.GOOGLE_SIGN_IN_CLIENT_SECRET}`
+}, async (profile, done) => {
+  
+  const user = {
+    name: profile.name,
+    email: profile.email,
+    id: profile.sub
+  }
+  done(undefined, user);
+}))
+
+passport.registerUserSerializer(async (user, req) => {
+  app.log.info({user}, "user serialization");
+  return user;
+});
+
+passport.registerUserDeserializer(async (user, req) => {
+  return user;
+})
+
 app.register(cors, {
   origin: `${process.env.CORS_DOMAIN}`.split(','),
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
-})
-  .register(multipart);
+});
+app.register(multipart).register(formbody);
 
 // routes
 app.register(adventureRoutes, {prefix: '/adventure'})
   .register(assetRoutes, { prefix: '/asset'})
-  .register(userRoutes, { prefix: '/user'});
+  .register(userRoutes, { prefix: '/user'})
+  .register(authRoutes, { prefix: '/auth'});
 
+export const log = app.log;
 export default app;
