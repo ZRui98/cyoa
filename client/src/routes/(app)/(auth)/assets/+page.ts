4f981478261168777df0cc100d00d8c360638e8e
Rui Zhu <ZRui98@users.noreply.github.com>
2023-10-27 13:35:31 -0400

    update auth routing (#35)

--- client/src/routes/(app)/(auth)/assets/+page.ts
+++ client/src/routes/(app)/(auth)/assets/+page.ts
@@ -1,20 +1,7 @@
-import { redirect } from '@sveltejs/kit';
 import { getAssets } from '../../../../utils/api.js';
-import { isApiError } from '@backend/utils/error';
 
 export async function load({ fetch }) {
-  try {
     const assets = await getAssets(fetch, true);
     return { assets };
-  } catch (e: unknown) {
-    let err = e as Error;
-    if (isApiError(err)) {
-      if (err.code === 403) {
-        throw redirect(308, '/activate');
-      } else if (err.code === 401) {
-        throw redirect(308, '/login');
-      }
-    }
-  }
 }
 
