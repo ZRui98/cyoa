f214bf5f72e062a8a86129b69074f61886d57bbe
Rui Zhu <ciguatera.z.rui@gmail.com>
2025-05-04 01:00:29 -0400

    switch to esm

--- client/src/routes/(app)/(public)/+layout.ts
+++ client/src/routes/(app)/(public)/+layout.ts
@@ -1,10 +1,10 @@
-import { browser } from '$app/environment';
+import { BROWSER } from 'esm-env';
 import { get } from 'svelte/store';
 import loginState from '../../../store/loginState';
 import { getUserStatus } from '../../../utils/api';
 
 export async function load({ fetch }) {
-  if (browser) {
+  if (BROWSER) {
     if (!get(loginState)) {
       const newLoginState = await getUserStatus(fetch);
       loginState.set(newLoginState);
